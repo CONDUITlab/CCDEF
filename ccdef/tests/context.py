bc5d640ddb08e2da3845a87c36815d3bd8cb03e4
prlaird <32419672+prlaird@users.noreply.github.com>
2020-11-16 20:47:48 -0500

    Create context.py

---
+++ ccdef/tests/context.py
@@ -0,0 +1,9 @@
+"""
+ccdef package tests
+
+"""
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+print (os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
